b82e22ae1893b4e1365b53a2cb2a9b777817d83a
omri <omri.ey@walkme.com>
2015-05-04 09:36:45 +0300

    iclude/exclude + replace

--- tasks/coverage-comparer.js
+++ tasks/coverage-comparer.js
@@ -16,11 +16,19 @@ module.exports = function(grunt) {
 		var delta = jsondiffpatch.diff(base, compared);
 
 		var filesArr = Object.keys(delta);
+		filesArr = replace(options, filesArr);
 
-		if (options.ignore) {
-			var ignore = JSON.parse(fs.readFileSync(options.ignore));
+		if (options.exclude) {
+			var exclude = getDataFromFile(options.exclude);
 			filesArr = filesArr.filter(function (item) {
-				return ignore.indexOf(item) == -1;
+				return !inArray(item, exclude);
+			});
+		}
+
+		if (options.include) {
+			var include = getDataFromFile(options.include);
+			filesArr = filesArr.filter(function (item) {
+				return inArray(item, include);
 			});
 		}
 
@@ -34,6 +42,40 @@ module.exports = function(grunt) {
 			console.log('Cannot write ' + options.outputFile + '\n\t' + err.message);
 		}
 	});
+
+	var types = {
+		json: {
+			parse: function (str) {
+				return JSON.parse(str);
+			}
+		},
+		text: {
+			parse: function (str) {
+				return str.split('\n');
+			}
+		}
+	};
+
+	function getDataFromFile(options) {
+		var data = fs.readFileSync(options.file).toString();
+		var parsed = types[options.type].parse(data);
+		parsed = replace(options, parsed);
+		return parsed;
+	}
+
+	function replace(options, data) {
+		if (options.replace) {
+			if (!options.replace.length) {
+				options.replace = [options.replace];
+			}
+			options.replace.forEach(function (rep) {
+				data = data.map(function (item) {
+					return item.replace(rep.src, rep.dest);
+				});
+			});
+		}
+		return data;
+	}
 
 	function parseFilesToJson(files) {
 		if (!files) {
@@ -52,5 +94,14 @@ module.exports = function(grunt) {
 			}
 			return obj;
 		}, {});
+	}
+
+	function inArray(str, arr) {
+		for (var i = 0; i < arr.length; i++) {
+			if (arr[i].indexOf(str) >= 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 };
